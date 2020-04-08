ce6e5cd2f5a4fda06686df6003c46b65575eff74
notlynoel <leo@notlinoel.com.br>
2020-04-08 00:41:50 -0300

    Adicionando o arquivo 01-impressao-especial.c

---
+++ 01-impressao-especial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main(void){
+
+	printf("1º Exercício \n");
+	
+	printf("Hello world!\n");
+        printf("Have fun with \"this\" course!");
+    
+        printf("2º Exercício \n");
+ 
+        printf("Léo Hollanda disse:\n \"A única maneira de aprender uma nova linguagem de programação é escrevendo programas nela\" \n");
+    
+        return 0;
+
+}
