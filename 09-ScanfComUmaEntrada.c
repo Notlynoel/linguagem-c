274eee1d1208e6e7ef581d03ce24d90ce60aa09d
notlynoel <leo@notlinoel.com.br>
2020-04-18 15:42:20 -0300

    Adicionando o arquivo 09-ScanfComUmaEntrada.c

---
+++ 09-ScanfComUmaEntrada.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int idade = 0;
+
+  printf("Qual a sua idade? \n");
+  scanf("%d", &idade);
+  printf("\nVocê tem %d anos de idade \n", idade);
+
+  return 0;
+
+}
